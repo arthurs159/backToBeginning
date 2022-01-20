ee0284077c9169c4e095747a19e9d5e3516efd49
Arthur Scarpati <arthur_159@hotmail.com>
2022-01-20 16:39:28 -0300

    Operações lógicas aninhadas

--- JavaClass/src/IfElse/FirstClassJava.java
+++ JavaClass/src/IfElse/FirstClassJava.java
@@ -3,31 +3,45 @@ package IfElse;
 public class FirstClassJava {
 
 	public static void main(String[] args) {
-		
+
 		int nota1 = 10;
 		int nota2 = 60;
-		int nota3 = 30;
-		int nota4 = 40;
+		int nota3 = 100;
+		int nota4 = 10;
 		int media = 0;
-		
+
 		media = (nota1 + nota2 + nota3 + nota4) / 4;
-		
+
 		/* Condições lógica com IF e ELSE */
-		
-		if (media >= 70) {
-			System.out.println("Aluno aprovado: " + media );
-		}else if (media >=40 && media <= 69) {
-			System.out.println("Aluno em recuperação: " + media);
-		}else {
-			System.out.println("Aluno Reprovado: " + media);
-		}
-		
+
+//		if (media >= 70) {
+//			System.out.println("Aluno aprovado: " + media );
+//		}else if (media >=40 && media <= 69) {
+//			System.out.println("Aluno em recuperação: " + media);
+//		}else {
+//			System.out.println("Aluno Reprovado: " + media);
+//		}
+
 		/* Operadores ternários são para micro validações */
-		
+
 //		String saidaResultado = media >= 70 ? "Aluno Aprovado" : "Aluno Reprovado";
-		String saidaResultado = media >= 70 ? "Aluno Aprovado" : (media >=40 && media <=69) ? "Aluno em recuperação" : "Aluno Reprovado";
-		System.out.println(saidaResultado);
-		
-		
+//		String saidaResultado = media >= 70 ? "Aluno Aprovado" : (media >=40 && media <=69) ? "Aluno em recuperação" : "Aluno Reprovado";
+//		System.out.println(saidaResultado);
+
+		/* Operações lógicas aninhadas: São pareções dentor de operações */
+
+		if (media >= 50) {
+			if (media >= 70) {
+				if (media > 90) {
+					System.out.println("O aluno está aprovado direto!! - Parabéns!!!");
+				} else {
+					System.out.println("Aluno está em recuperação");
+				}
+			} else {
+				System.out.println("Aluno em recuperação");
+			}
+		} else {
+			System.out.println("Aluno reprovado direto!!");
+		}
 	}
 }
