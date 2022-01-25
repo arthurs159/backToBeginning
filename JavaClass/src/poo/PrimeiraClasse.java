63595936b75f88c10b860457212cf527988e33aa
Arthur Scarpati <arthur_159@hotmail.com>
2022-01-25 15:39:30 -0300

    Metodos aprovado ou não

--- JavaClass/src/poo/PrimeiraClasse.java
+++ JavaClass/src/poo/PrimeiraClasse.java
@@ -3,14 +3,29 @@ package poo;
 public class PrimeiraClasse {
 
 	public static void main(String[] args) {
-		
-		/*Objeto ainda não existe na memoria*/
-//		Aluno aluno1;
-		
-//		/*Agora temos um objeto real na memoria*/
-//		Aluno aluno2 = new Aluno();
-		
+
+		/*aluno1 é uma referencia para o objeto aluno*/
 		Aluno aluno1 = new Aluno(); /*Aqui será Joao*/
+		aluno1.setNome("João");
+		aluno1.setIdade(50);
+		aluno1.setDataNascimento("18/10/1987");
+		aluno1.setRegistroGeral("454.4545.4554");
+		aluno1.setNumeroCpf("1569.45454.32326");
+		aluno1.setNomeMae("Shirley");
+		aluno1.setNomePai("Antonio");
+		aluno1.setDataMatricula("10/10/2020");
+		
+		aluno1.setNota1(90);
+		aluno1.setNota2(80.8);
+		aluno1.setNota3(70.9);
+		aluno1.setNota4(90.7);
+		
+		System.out.println("Nome é = " + aluno1.getNome());
+		System.out.println("Idade é = " + aluno1.getIdade());
+		System.out.println("Nascimento = " + aluno1.getDataNascimento());
+		System.out.println("Média da nota é = " + aluno1.getMediaNota());
+		System.out.println("Resultado = " + (aluno1.getAlunoAprovado() ? "Aprovado" : "Reprovado"));
+		System.out.println("Resultado 2 = " + aluno1.getAlunoAprovado2());
 		
 		Aluno aluno2 = new Aluno(); /*Aqui será Pedro*/
 		
