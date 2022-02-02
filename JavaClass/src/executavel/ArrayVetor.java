cf6f95d285681761d9ce6a5009b6efa742659876
Arthur Scarpati <arthur_159@hotmail.com>
2022-02-02 16:37:35 -0300

    Trabalhando com Matriz bidimensional

--- JavaClass/src/executavel/ArrayVetor.java
+++ JavaClass/src/executavel/ArrayVetor.java
@@ -13,6 +13,10 @@ public class ArrayVetor {
 		Aluno aluno = new Aluno();
 		aluno.setNome("Alex Fernando");
 		aluno.setNomeEscola("JDEV Treinamentos");
+		
+		Aluno aluno2 = new Aluno();
+		aluno2.setNome("Alex Fernando 2");
+		aluno2.setNomeEscola("JDEV Treinamentos");
 
 		// Criação da disciplina
 		Disciplina disciplina = new Disciplina();
@@ -24,21 +28,43 @@ public class ArrayVetor {
 		Disciplina disciplina2 = new Disciplina();
 		disciplina2.setDisciplina("Lógica de programação");
 		disciplina2.setNota(notasLogica);
-		aluno.getDisciplinas().add(disciplina2);
-
-		System.out.println("Nome do aluno = " + aluno.getNome() + ", Inscrito no curso : " + aluno.getNomeEscola());
-		System.out.println(" ----- Disciplina do aluno ----- ");
 		
-		for (Disciplina disc : aluno.getDisciplinas()) {
+		aluno.getDisciplinas().add(disciplina2);
+		
+		Disciplina disciplina3 = new Disciplina();
+		disciplina3.setDisciplina("Lógica de programação");
+		disciplina3.setNota(notasLogica);
+		
+		aluno2.getDisciplinas().add(disciplina3);
+		
+		Disciplina disciplina4 = new Disciplina();
+		disciplina4.setDisciplina("Lógica de programação");
+		disciplina4.setNota(notasLogica);
+		
+		aluno2.getDisciplinas().add(disciplina4);
 
-			System.out.println("Disciplina: " + disc.getDisciplina());
-			System.out.println("As notas da disciplina são: ");
+		// ---------------------------------------------------- //
 
-			for (int i = 0; i < disc.getNota().length; i++) {
-				System.out.println("Nota " + (i+1) + " é igual = " + disc.getNota()[i]);
+		Aluno[] arrayAlunos = new Aluno[2];
+		
+		arrayAlunos[0] = aluno;
+		arrayAlunos[1] = aluno2;
+		
+		for (int i=0; i<arrayAlunos.length; i++) {
+			
+			System.out.println("Nome do aluno é: " + arrayAlunos[i].getNome());
+			
+			for(Disciplina d : arrayAlunos[i].getDisciplinas()) {
+				System.out.println("Nome da disciplina é: " + d.getDisciplina());
+				
+				for(int j=0; j<d.getNota().length; j++) {
+					System.out.println("A nota número: " + (j+1) + " é igual = " + d.getNota()[j]);
+				}
 			}
-
+			
+			
+			
 		}
-
+		
 	}
 }
