9dae402c878f3e4a6a0c96210ca8525f1650b062
Arthur Scarpati <arthur_159@hotmail.com>
2022-02-03 16:55:13 -0300

    Organizando arquivo e começando com Thread

---
+++ JavaClass/src/thread/Threads.java
@@ -0,0 +1,13 @@
+package thread;
+
+public class Threads {
+	public static void main(String[] args) throws InterruptedException {
+
+		
+		for (int i = 0; i < 10; i++) {
+			
+			Thread.sleep(1000);
+			System.out.println("Executando rotina, envio de e-mail");
+		}
+	}
+}
