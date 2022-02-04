edd93089abc4035c8e626b97e205c1f2c4e725f7
Arthur Scarpati <arthur_159@hotmail.com>
2022-02-04 15:51:38 -0300

    Entendendo Threads

--- JavaClass/src/thread/Threads.java
+++ JavaClass/src/thread/Threads.java
@@ -1,13 +1,58 @@
 package thread;
 
+import javax.swing.JOptionPane;
+
 public class Threads {
 	public static void main(String[] args) throws InterruptedException {
 
-		
-		for (int i = 0; i < 10; i++) {
-			
-			Thread.sleep(1000);
-			System.out.println("Executando rotina, envio de e-mail");
-		}
+		Thread threadEmail = new Thread(thread1);
+		threadEmail.start();
+
+		Thread threadFiscal = new Thread(thread2);
+		threadFiscal.start();
+
+		System.out.println("CHEGOU AO FIM DO CÓDIGO DE TESTE DE THREAD");
+
+		JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário");
+
 	}
+
+	private static Runnable thread1 = new Runnable() {
+
+		@Override
+		public void run() {
+			/* Código da rotina que eu quero execurar em paralelo */
+			for (int i = 0; i < 10; i++) {
+
+				System.out.println("Executando rotina, envio de e-mail");
+
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	};
+
+	private static Runnable thread2 = new Runnable() {
+
+		@Override
+		public void run() {
+			/* Código da rotina que eu quero execurar em paralelo */
+			for (int i = 0; i < 10; i++) {
+
+				System.out.println("Executando rotina, envio de Nota fiscal!!!");
+
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
+
+	};
+
 }
